e2a11d0501007f5a8aefb1a93cec99ede4626ce5
AnhPham <anh.phamdoduc@nashtechglobal.com>
2017-07-02 11:35:20 +0100

    Add project files.

---
+++ DesignPatternPractice/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Composite
+{
+    class Employee
+    {
+        private string Name { get; set; }
+        private string Department { get; set; }
+        private decimal Salary { get; set; }
+        private List<Employee> Subordinates { get; set; }
+
+        public Employee(string name, string department, decimal salary) {
+            Name = name;
+            Department = department;
+            Salary = salary;
+            Subordinates = new List<Employee>();
+        }
+
+        public void AddSubordinate(Employee emp) {
+            Subordinates.Add(emp);
+        }
+
+        public void RemoveSubordinate(Employee emp) {
+            Subordinates.Remove(emp);
+        }
+
+        public List<Employee> GetSubordinates() {
+            return Subordinates;
+        }
+
+        public override string ToString() => $"Name: {Name}, Department: {Department}, Salary: {Salary}";
+    }
+}
---
+++ Observer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const byte temp = (byte)12;
+
+            Console.WriteLine(temp);
+
+            Console.ReadLine();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
